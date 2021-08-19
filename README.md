7099db994fee75020051799bdd870648de12b2c6
ezeltinger <ezeltinger@gmail.com>
2021-08-18 23:20:43 -0500

    chore: add float conversion

--- README.md
+++ README.md
@@ -7,3 +7,11 @@ Converts numbers between decimal, hex, binary, and octal.
 1. In the scripts directory run `create_venv.bat` to build the python venv.
 2. Then run the command `pyinstaller number_converter.py --onefile --noconsole`
 3. Use the executable in dist/ to start the Number Converter program.
+
+## User Guide
+
+The executable can be found at <https://github.com/ezeltinger/NumberConverter/releases> or in the dist/ folder of the repo after pyinstaller is run.
+
+Once you have Number Converter open, you can add a number to any of the entries and then click 'Convert' or hit enter to convert to other numeral systems.
+
+To clear all entries, click 'Clear' or hit enter when two or more entries are filled.
--- number_converter.py
+++ number_converter.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
+from typing import List, Tuple
+from functools import partial
+import struct
 
 class Numeral:
     """Contains an object with a name, numeral base, and tkinter Label and Entry objects
@@ -10,9 +14,9 @@ class Numeral:
         self.entry = tk.Entry(master=window)
 
 
-def handle_conversion():
+def convert_integer():
     entry_count = 0
-    for numeral in numerals:
+    for numeral in int_numerals:
         if numeral.entry.get():
             entry_count = entry_count + 1
             converting_base = numeral.base
@@ -20,9 +24,7 @@ def handle_conversion():
     if entry_count > 1:
         message["text"] = "More than one entry filled!"
     else:
-        # hex_entry.insert(0, hex(decimal))
-        # binary_entry.insert(0, bin(decimal))
-        for numeral in numerals:
+        for numeral in int_numerals:
             if numeral.base != converting_base:
                 if numeral.base == 2:
                     numeral.entry.insert(0, f'{number:b}')
@@ -33,45 +35,84 @@ def handle_conversion():
                 elif numeral.base == 8:
                     numeral.entry.insert(0, f'{number:o}')
 
-def clear_entries():
+def convert_float():
+    entry_count = 0
+    for numeral in float_numerals:
+        if numeral.entry.get():
+            entry_count = entry_count + 1
+            converting_base = numeral.base
+            if converting_base == 16:
+                hex_string = numeral.entry.get()
+            elif converting_base == 10:
+                number = float(numeral.entry.get())
+    if entry_count > 1:
+        message["text"] = "More than one entry filled!"
+    else:
+        for numeral in float_numerals:
+            if numeral.base != converting_base:
+                if numeral.base == 16:
+                    hex_string = hex(struct.unpack('<I', struct.pack('<f', number))[0])
+                    hex_string = hex_string[2:]
+                    numeral.entry.insert(0, hex_string)
+                if numeral.base == 10:
+                    number = struct.unpack('!f', bytes.fromhex(hex_string))[0]
+                    numeral.entry.insert(0, f'{number}')
+
+def clear_entries(numerals):
     for numeral in numerals:
         numeral.entry.delete(0, 'end')
 
 def handle_keypress(event):
-    entry_count = sum(1 for numeral in numerals if numeral.entry.get())
-    if entry_count > 1:
-        clear_entries()
-    elif entry_count == 1:
-        handle_conversion()
+    int_entry_count = sum(1 for numeral in int_numerals if numeral.entry.get())
+    if int_entry_count > 1:
+        clear_entries(int_numerals)
+    elif int_entry_count == 1:
+        convert_integer()
+    float_entry_count = sum(1 for numeral in float_numerals if numeral.entry.get())
+    if float_entry_count > 1:
+        clear_entries(float_numerals)
+    elif float_entry_count == 1:
+        convert_float()
+
+
+def converter_layout(row_number, numeral_tuples) -> Tuple[List[Numeral], int]:
+    numerals = []
+
+    # Create Numeral object for each numeral in numeral_tuples list and put it in numerals list
+    for name, base in numeral_tuples:
+        numerals.append(Numeral(name=name, window=window, base=base))
+
+    # Place the label and entry widgets for each Numeral object
+    for numeral in numerals:
+        numeral.label.grid(row=row_number, column=0, padx=5, pady=5)
+        numeral.entry.grid(row=row_number, column=1, padx=5, pady=5)
+        row_number = row_number + 1
+    
+    return numerals, row_number
 
 
 
-numeral_tuples = [
+int_tuples = [
     ("Decimal", 10),
     ("Hexadecimal", 16),
     ("Binary", 2),
     ("Octal", 8)
 ]
-numerals = []
+
+float_tuples = [
+    ("Decimal Float", 10),
+    ("Hex Float", 16)
+]
+
+total_rows = len(int_tuples) + len(float_tuples) + 5
 
 window = tk.Tk()
 window.title("Number Converter")
 
-window.columnconfigure(list(range(2)), weight=1, minsize=150)
-window.rowconfigure(list(range(len(numeral_tuples)+2)), weight=1, minsize=50)
+window.columnconfigure(list(range(2)), weight=1, minsize=100)
+window.rowconfigure(list(range(total_rows)), weight=1, minsize=25)
 
-# Create Numeral object for each numeral in numeral_tuples list and put it in numerals list
-for name, base in numeral_tuples:
-    numerals.append(Numeral(name=name, window=window, base=base))
-
-# Place the label and entry widgets for each Numeral object
-for numeral in numerals:
-    numeral.label.grid(row=numerals.index(numeral), column=0, padx=5, pady=5)
-    numeral.entry.grid(row=numerals.index(numeral), column=1, padx=5, pady=5)
-
-# Create and place a message widget below the numerals
-message = tk.Label(master=window)
-message.grid(row=4, column=0, columnspan=2, padx=5, pady=5)
+int_numerals, current_row = converter_layout(row_number=0, numeral_tuples=int_tuples)
 
 # Add a button for converting numbers
 convert_button = tk.Button(
@@ -79,10 +120,11 @@ convert_button = tk.Button(
     text="Convert",
     width=8,
     height=1,
-    command=handle_conversion
+    command=convert_integer
 )
-convert_button.grid(row=5, 
-                    column=0, 
+convert_button.grid(row=current_row, 
+                    column=0,
+                    padx=5,
                     sticky="ew")
 
 # Add a button for clearing the entries
@@ -91,11 +133,56 @@ clear_button = tk.Button(
     text="Clear",
     width=8,
     height=1,
-    command=clear_entries
+    command=partial(clear_entries,int_numerals)
 )
-clear_button.grid(row=5, 
-                    column=1, 
+clear_button.grid(row=current_row, 
+                    column=1,
+                    padx=5,
                     sticky="ew")
+current_row += 1
+
+separator = ttk.Separator(window, orient='horizontal')
+separator.grid(row=current_row, columnspan=2, sticky='ew')
+
+current_row += 1
+
+# ttk.Label(window, text='Float Converter').grid(row=current_row, columnspan=2)
+# current_row += 1
+
+float_numerals, current_row = converter_layout(row_number=current_row, numeral_tuples=float_tuples)
+all_numerals = int_numerals + float_numerals
+
+# Add a button for converting floats
+convert_float_button = tk.Button(
+    master=window, 
+    text="Convert Float",
+    width=8,
+    height=1,
+    command=convert_float
+)
+convert_float_button.grid(row=current_row, 
+                    column=0,
+                    padx=5,
+                    sticky="ew")
+
+# Add a button for clearing the float entries
+clear_float_button = tk.Button(
+    master=window,
+    text="Clear",
+    width=8,
+    height=1,
+    command=partial(clear_entries,float_numerals)
+)
+clear_float_button.grid(row=current_row, 
+                    column=1,
+                    padx=5,
+                    sticky="ew")
+current_row += 1
+
+# Create and place a message widget below the numerals
+message = ttk.Label(master=window)
+message.grid(row=current_row, column=0, columnspan=2, padx=5, pady=5)
+current_row += 1
 
 # Use the enter key to convert or clear entries
 window.bind("<Return>", handle_keypress)
